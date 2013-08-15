babc876c9b177538aec79e4674c0df110a4343ec
Tim Cameron Ryan <id@timryan.org>
2013-08-14 20:01:24 -0400

    Wait for it

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hardware');
+#!/usr/bin/env node
+
+process.stdout.write('>.> wait for it... ');
+
+setInterval(function () {
+  process.stdout.write('\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b<.< wait for it... ')
+}, 1000);
+
+setTimeout(function () {
+  setInterval(function () {
+    process.stdout.write('\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b>.> wait for it... ')
+  }, 1000);
+}, 500);
