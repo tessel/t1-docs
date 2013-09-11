157240875915ff4c4b08f2e9c360fe5389b333ac
Tim Cameron Ryan <id@timryan.org>
2013-09-11 14:14:15 -0400

    Hardware defines its own interface

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-#!/usr/bin/env node
+/**
+ * By default, support on this module exists only for Firmata.
+ */
 
-console.log('10 GOTO https://github.com/technicalmachine/hardware\n20 GOTO 10')
+exports.firmata = function (board) {
+  return {
+    pinOutput: function (idx) {
+      board.pinMode(idx, board.MODES.OUTPUT);
+    },
+    digitalWrite: function (idx, val) {
+      board.digitalWrite(idx, val ? board.HIGH : board.LOW);
+    },
+    I2C: function (interface, addr) {
+      this.initialize = function () {
+        board.sendI2CConfig();
+      }
+      this.transfer = function (send, recv, next) {
+        board.sendI2CWriteRequest(addr, send);
+        board.sendI2CReadRequest(addr, recv, function (data) {
+          next(null, data);
+        });
+      }
+      this.read = function (recv, next) {
+        board.sendI2CReadRequest(addr, recv, function (data) {
+          next(null, data);
+        });
+      }
+      this.send = function (send, next) {
+        board.sendI2CWriteRequest(addr, send);
+        setImmediate(next, null);
+      }
+    }
+  }
+};
